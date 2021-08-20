a0d53f99b88bf006967feabdfa3a71885fb8b556
roshan <roshankumarsharmamika@gmail.com>
2021-08-20 18:00:58 +0530

    added joi validation for posts

--- api/v1/posts/posts.validation.js
+++ api/v1/posts/posts.validation.js
@@ -1,1 +1,10 @@
 // Add JOI validation here
+const Joi = require('joi')
+
+const postSchema = Joi.object({
+  htmlContent: Joi.string().required(),
+  tags: Joi.array().items(Joi.string().required()).required(),
+  postedBy: Joi.string().required(),
+})
+
+module.exports = postSchema
