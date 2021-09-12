5654b712c9869fab958684cfb15cffe37d281894
taurtk1 <taurtk@gmail.com>
2021-09-12 20:44:57 +0530

    adding user login history

--- api/v1/users/users.events.js
+++ api/v1/users/users.events.js
@@ -1,9 +1,11 @@
 const EventEmitter = require('events')
 const path = require('path')
 const ejs = require('ejs')
+const createHttpError = require('http-errors')
 const mailer = require('../../../configs/email')
 const { signAccessToken } = require('../../../utils/token')
 const env = require('../../../configs/config')
+const userLoginHistory = require('./usersLoginHistory.model')
 
 const eventEmitter = new EventEmitter()
 
@@ -46,5 +48,42 @@ eventEmitter.on('sendResetPasswordEmail', async (req, metadata) => {
 
   await mailer.sendEmail(mailOption)
 })
+
+eventEmitter.on(
+  'userLoginHistory',
+  async (userRequestInfo) => {
+    try {
+      await userLoginHistory.findOneAndUpdate(
+        { username: userRequestInfo.username },
+        userRequestInfo,
+        {
+          new: true,
+          upsert: true, // Make this update into an upsert
+        }
+      )
+      /* continue as normal */
+    } catch (error) {
+      throw createHttpError.InternalServerError(error)
+    }
+  }
+
+  // const token = await signAccessToken(req.payload, '1h', metadata)
+
+  // const link = `http://${req.headers.host}${req.baseUrl}/email/verification/${token}`
+
+  // const data = await ejs.renderFile(
+  //   path.join(__dirname, '../../..', 'views/email_verification.ejs'),
+  //   { link }
+  // )
+
+  // const mailOption = {
+  //   to: req.payload.email,
+  //   from: env.SENDER_EMAIL,
+  //   text: token,
+  //   html: data,
+  // }
+
+  // await mailer.sendEmail(mailOption)
+)
 
 module.exports = eventEmitter
