e5ed96ce06ff2323b1cb3449764432a81bb5007d
Shubham <34097422+shubdev07@users.noreply.github.com>
2021-10-02 23:04:57 +0530

    Add redis password config

--- configs/config.js
+++ configs/config.js
@@ -11,6 +11,7 @@ const config = {
     host: process.env.REDIS_HOST,
     port: process.env.REDIS_PORT,
     db: process.env.REDIS_DB || 0,
+    password: process.env.REDIS_PASSWORD
   },
   emailRetryLimit: 3,
   logDir: 'logs',
